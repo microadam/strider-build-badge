708b4e0846f597519fdbb00a6e8599bf66f9c639
Adam Bogdal <adam@bogdal.pl>
2014-11-06 02:19:17 +0100

    Add tests

--- test/build-status.test.js
+++ test/build-status.test.js
@@ -7,7 +7,12 @@ describe('Build Status', function () {
     var jobModel =
     { findOne: function () { return jobModel }
     , sort: function () { return jobModel }
-    , where: function () { return jobModel }
+    , where: function (field, value) {
+        if(field === 'ref.branch' && value !== 'master') {
+          job = null;
+        }
+        return jobModel
+      }
     , ne: function () { return jobModel }
     , exec: function (callback) { callback(null, job) }
     }
@@ -16,7 +21,7 @@ describe('Build Status', function () {
 
   it('should return success image with successful build', function (done) {
     var job = { test_exitcode: 0 }
-    getBuildStatus('test', getContext(job), function (error, imagePath) {
+    getBuildStatus('test', 'master', getContext(job), function (error, imagePath) {
       expect(imagePath).to.be('build_passing.png')
       done()
     })
@@ -24,17 +29,32 @@ describe('Build Status', function () {
 
   it('should return failed image with failed build', function (done) {
     var job = { test_exitcode: 1 }
-    getBuildStatus('test', getContext(job), function (error, imagePath) {
+    getBuildStatus('test', 'master', getContext(job), function (error, imagePath) {
       expect(imagePath).to.be('build_failing.png')
       done()
     })
   })
 
   it('should return unknown image when build state unknown', function (done) {
-    getBuildStatus('test', getContext(null), function (error, imagePath) {
+    getBuildStatus('test', 'master', getContext(null), function (error, imagePath) {
       expect(imagePath).to.be('build_unknown.png')
       done()
     })
   })
 
+  it('should return success image for the default branch ', function (done) {
+    var job = { test_exitcode: 0 }
+    getBuildStatus('test', undefined, getContext(job), function (error, imagePath) {
+      expect(imagePath).to.be('build_passing.png')
+      done()
+    })
+  })
+
+  it('should return unknown image when job for the specified branch does not exist', function (done) {
+    var job = { test_exitcode: 0 }
+    getBuildStatus('test', 'develop', getContext(job), function (error, imagePath) {
+      expect(imagePath).to.be('build_unknown.png')
+      done()
+    })
+  })
 })
